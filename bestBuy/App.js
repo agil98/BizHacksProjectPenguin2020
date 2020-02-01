fe93eb1a50138646bfd1e806323e7da2e7081c5a
Alexandra Gil <alexandra.gil@hotmail.ca>
2020-02-01 11:21:15 -0800

    Removed MainPage and View inside App.js

--- bestBuy/App.js
+++ bestBuy/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-//import MainPage from "./components/MainPage";
 import QRCodeScanner from "./components/QRCodeScanner";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Project Penguin!!!!!!!!!!!</Text>
       <MainPage />
       <QRCodeScanner />
-    </View>
   );
 }
 
