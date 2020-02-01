4047ba23da8dae5505ffaf2e29731e074fa3e3ad
john <likisongproduct@gmail.com>
2020-02-01 12:04:47 -0800

    refactored receipt

--- bestBuy/App.js
+++ bestBuy/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import QRScanner from "./components/QRScanner";
 import { Button, Header, Overlay } from "react-native-elements";
+import Receipt from "./Receipt";
 
 export default class App extends Component {
   constructor(props) {
@@ -21,29 +22,7 @@ export default class App extends Component {
           centerComponent={{ text: "OmniPay", style: { color: "#fff" } }}
           rightComponent={{ icon: "home", color: "#fff" }}
         />
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "#fff",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <Overlay
-            isVisible={this.state.isVisible}
-            onBackdropPress={() => this.setState({ isVisible: false })}
-            style={{ flex: 1, justifyContent: "center" }}
-          >
-            <Text>Purchase Confirmation </Text>
-            <View style={{ height: 100, paddingVertical: 100 }}>
-              <Image
-                style={{ width: 250, height: 150 }}
-                source={require("./assets/dell.jpg")}
-              />
-              <Text>Dell XPS 13</Text>
-            </View>
-          </Overlay>
-        </View>
+        <Receipt></Receipt>
 
         <View style={styles.title}>
           <Text style={styles.text}>
