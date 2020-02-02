74df3afa5c86bbc5e19ecb4d1e6802fa78ecc7db
rorozco1997 <44987991+rorozco1997@users.noreply.github.com>
2020-02-01 18:00:24 -0800

    Update purchase.js

--- server/routes/purchase.js
+++ server/routes/purchase.js
@@ -16,10 +16,10 @@ router.post('/registerOffer', function(req, res, next) {
 
 function generateOffers(res) {
     let prod = { uuid: 2250, prodName: "Corsair Keyboard", type: 0, description: "Corsair mechanical " +
-            "keyboard to go along with your Dell XPS 13!", promoKey: "Holiday Season Special Sale", offerID: 9999, pid: 1234, stid: 778, price: 150, status: "true" };
-    let serv = { uuid: 2250, prodName: "GeekSquad Support", type: 1, description: "Get 2 years of geek squad support " +
-            "on your new DELL XPS 13!", promoKey: "Black Friday Special Sale", offerID: 9999, pid: 1234, stid: 778, price: 200, status: "true" };
-    let arr = [prod, serv];
+            "keyboard to go along with your Dell XPS 13!", promoKey: "Holiday Season Special Sale", offerID: 9999,
+        pid: 1234, stid: 778, price: 150, status: "true" };
+
+    let arr = [prod];
 
     res.json(arr);
 }
